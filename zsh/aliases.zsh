8ba9d67de83e7c16f1f414bf0cf44a6e6ee3bce6
DeanXu <xdx158@gmail.com>
2014-03-18 18:08:41 +0800

    update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,6 @@
 alias reload!='. ~/.zshrc'
 alias tmuxa='tmux attach'
 alias mt-bd-dir="sshfs docker@localhost:/mnt/sda1/workspace ~/Desktop/workspace -oping_diskarb,volname=b2d-workspace -p 2022 -o reconnect -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o password_stdin < ~/.boot2docker/b2d-passwd"
-alias unmt-bd-dir="unmount ~/Desktop/workspace"
+alias unmt-bd-dir="umount ~/Desktop/workspace"
+alias towork='cd ~/Desktop/workspace/trunk/japp-diy/'
+alias build='docker start -i db4bf93ccb5e'
